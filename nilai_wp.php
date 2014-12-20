2b566001798f850f68c1b8d78f866fd820b4102e
samsul <el.bachrie976@gmail.com>
2014-12-20 14:22:51 +0700

    Nilai WP hampir FIX, bug: kriteria (alias) yg dimunculkan belum berdasarkan user -- Mr. Sam

--- nilai_wp.php
+++ nilai_wp.php
@@ -20,7 +20,14 @@
 	
 	$tampil_alternatif = "SELECT * FROM `alternatif` WHERE `id_wilayah` = ".$id_wilayah;
 	$tampil_query = mysql_query($tampil_alternatif);	
+
+
+	$id_kriteria = 0;
 	
+	$query = "SELECT alias FROM kriteria";
+	$tampil_query2 = mysql_query($query)or die (mysql_error());	
+
+
 ?>
 <html>
 <head>
@@ -50,9 +57,8 @@
 </head>
 <body>
 
-
 	<div class="row" style="margin-top: 50pt">
-		<div class="panel panel-primary" style="text-align: center">
+		<div class="col-sm-offset-1 col-sm-10 panel panel-primary" style="text-align: center">
 			<div class="panel-heading">	
 				<h1>Weighted Product</h1><br>
 			</div><br>
@@ -72,124 +78,57 @@
 			<div class="panel-body">
 				<div class="row" style="margin-top: 10pt">
 					<div class="col-sm-12" style="text-align: center;">
-						<form action="proses_simpan_bobot_wp.php" method="POST">
+						<form action="proses_hitung_wp.php" method="POST">
 							<table border="1" cellpadding="5" cellspacing="5" class="display" id="tabel_wilayah">
-							<thead  text-align="center" >
-		
-		<tr>
-			<th rowspan="2">No.</th>
-			<th rowspan="2">Alternatif</th>
-			<th colspan="11">Kriteria</th>
-		</tr>
+							<thead  text-align="center">
+								<tr>
+									<th rowspan="2" style="text-align: center;">Alternatif (No.SPT)</th>
+									<th colspan="11" style="text-align: center;">Kriteria</th>
+								</tr>
 
-		<tr>
-			<td>C1</td>
-			<td>C2</td>
-			<td>C3</td>
-			<td>C4</td>
-			<td>C5</td>
-			<td>C6</td>
-			<td>C7</td>
-			<td>C8</td>
-			<td>C9</td>
-			<td>C10</td>
-			<td>C11</td>
-		</tr>
-		
-		<tr>
-			<td>1</td>
-			<td>3</td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-		</tr>
-		
-		<tr>
-			<td>2</td>
-			<td>16</td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-		</tr>
-
-		<tr>
-			<td>3</td>
-			<td>4</td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-		</tr>
-
-		<tr>
-			<td>4</td>
-			<td>27</td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-			<td style=\"text-align: center;\"><input type='text' class='form-control' id='nilai' name='nilai'></td>
-		</tr>
+								<tr>
+<?php
+	while($tampil_hasil2 = mysql_fetch_array($tampil_query2))
+	{
+		$alias = $tampil_hasil2['alias'];
+		echo"<td>$alias</td>";
+	}		
+?>
+								</tr>
 							</thead>
-							<tbody>						
+
+							<tbody>	
+							<tr>					
 <?php 
-	$nomor = 1;
+	$jlh_kriteria = mysql_num_rows($tampil_query2);
+
 	while($tampil_hasil = mysql_fetch_array($tampil_query))
 	{
-		$alternatif = $tampil_hasil['alternatif'];
-		$id = $tampil_hasil['id_alternatif'];
+		$alternatif = $tampil_hasil['no_spt'];
 		
-		echo"<tr>
-						<td style=\"text-align: center;\">$nomor</td>
-						<td>$alternatif</td>
-						
-					</tr>";
-		$nomor++;
+		echo"<td style=\"text-align: center;\">$alternatif</td>";
+
+		for($i = 0; $i < $jlh_kriteria; $i++) {
+			echo "<td style=\"text-align: center;\"> <input type='text' class='form-control' id='nilai' name='nilai'> </td>";
+		}
+
+		echo "</tr>";
 	}
 ?>	
+							
 							</tbody>
-						</table>
-						<div class="row">
-						<div style="text-align: center">
-						    <input type="submit" class="btn btn-success" value="Hitung">
-						    <a role="button" class="btn btn-default" href="metode.php">Batal</a>
-						</div>
-						</div>
-					</form>
+							</table><br>
+			<div class="row">
+				<div style="text-align: center">
+					<input type="submit" class="btn btn-success" value="Hitung">
+					<a role="button" class="btn btn-default" href="wp.php">Batal</a>
+				</div>
+			</div>
+						</form>
 					</div>
 				</div>		
 			</div>
 		</div>	
 	</div>
-
 </body>
 </html>
